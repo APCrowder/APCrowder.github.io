ac2d471804367dc66f9cc2f118ec3839ba363ea4
Adrian Crowder <adrian_crowder@live.com>
2019-10-09 10:31:30 -0700

    SmoothScrolling

--- scrollToAnchor.js
+++ scrollToAnchor.js
@@ -2,7 +2,15 @@ $(document).ready(function (){
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+            var curScroll = $(window).scrollTop();
+            var toScroll = $(this.hash).offset().top - 72;
+            
+            var scrollTime = 15 * Math.sqrt(Math.abs(curScroll - toScroll)) + 100;
+            
+            $('html, body').animate({
+               scrollTop: $(this.hash).offset().top - 72
+            }, scrollTime, 'swing');
+            // document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
         });
     });
 });
